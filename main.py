508626753262ed9fd79eebd40d89a3a2d6cd0d02
Pedro Costa <pedrohdcosta@outlook.com>
2022-06-18 13:55:46 -0300

    Adicionando a interface do cmd

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# apenas um teste
+import keyboard
+
+from interface import interface
+
+if __name__ == '__main__':
+        interface(0, 0, 0, 0)
+        print(f'\nTecla pressionada: {keyboard.read_key()}')
+        
+        if keyboard.is_pressed('esc'):
+            print("\n" * 30)
